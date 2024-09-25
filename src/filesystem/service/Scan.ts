3a83c0e28d8b6cb85666c05dc4a2076786a631cd
Thomas Cicognani <thomas.cicognani@proton.me>
2024-09-25 17:56:34 +0200

    refactor: simplify scan service

--- src/filesystem/service/Scan.ts
+++ src/filesystem/service/Scan.ts
@@ -2,28 +2,15 @@ import type { Directory } from "../domain/valueobject/Directory.ts";
 import type { File } from "../domain/valueobject/File.ts";
 import type { FileType } from "../domain/valueobject/FileType.ts";
 import type { FilesRepository } from "../repository/FilesRepository.ts";
-import type { ScanFiles } from "./ScanFiles.ts";
 
 export class Scan {
-  constructor(
-    private readonly scanFileService: ScanFiles,
-    private readonly repository: FilesRepository,
-  ) {}
+  constructor(private readonly repository: FilesRepository) {}
 
   public async scanAndSave(
     directoryToScan: Directory,
     type: FileType,
   ): Promise<void> {
-    const filesMap: Map<Directory, File[]> = await this.scanFileService.scan([
-      directoryToScan,
-    ]);
-
-    for (const [_, files] of filesMap) {
-      await this.saveFiles(files, type);
-    }
-  }
-
-  public async saveFiles(files: File[], type: FileType): Promise<void> {
+    const files: File[] = await directoryToScan.scanDirectories();
     await this.repository.saveFiles(files, type);
   }
 }
--- test/filesystem/domain/valueobject/Directory.test.ts
+++ test/filesystem/domain/valueobject/Directory.test.ts
@@ -1,5 +1,13 @@
-import { assert, assertFalse, assertThrows } from "jsr:@std/assert";
+import {
+  assert,
+  assertArrayIncludes,
+  assertEquals,
+  assertFalse,
+  assertMatch,
+  assertThrows,
+} from "jsr:@std/assert";
 import { Directory } from "../../../../src/filesystem/domain/valueobject/Directory.ts";
+import type { File } from "../../../../src/filesystem/domain/valueobject/File.ts";
 import { Path } from "../../../../src/filesystem/domain/valueobject/Path.ts";
 
 Deno.test(function equals() {
@@ -21,4 +29,43 @@ Deno.test(function notEquals() {
 
 Deno.test(function notDirectory() {
   assertThrows(() => new Directory(new Path("LICENSE")));
+});
+
+Deno.test(async function scanDirectoryRecursively() {
+  // GIVEN
+  const directory = new Directory(new Path("./test/resources"));
+
+  // WHEN
+  const files: File[] = await directory.scanDirectories();
+
+  // THEN
+  assertEquals(files.length, 5);
+
+  const filesPaths: string[] = files.map((file) => file.path.value);
+
+  assertArrayIncludes(filesPaths, [
+    "./test/resources/video-game/8-Bit Bayonetta (2015)/PC/8-Bit Bayonetta - 00001.webp",
+  ]);
+  assertArrayIncludes(filesPaths, [
+    "./test/resources/video-game/8-Bit Bayonetta (2015)/PC/8-Bit Bayonetta - 00002.webp",
+  ]);
+  assertArrayIncludes(filesPaths, [
+    "./test/resources/video-game/80's Overdrive (2017)/Nintendo Switch/80's Overdrive - 00001.webp",
+  ]);
+  assertArrayIncludes(filesPaths, [
+    "./test/resources/video-game/80's Overdrive (2017)/Nintendo Switch/80's Overdrive - 00005.webp",
+  ]);
+  assertArrayIncludes(filesPaths, [
+    "./test/resources/video-game/80's Overdrive (2017)/Nintendo Switch/80's Overdrive - 00006.webp",
+  ]);
+
+  const filesChecksums: Promise<string>[] = files.map((file) =>
+    file.getChecksum(),
+  );
+
+  Promise.all(filesChecksums).then((checksums) => {
+    for (const checksum of checksums) {
+      assertMatch(checksum, /^[0-9a-f]{128}$/);
+    }
+  });
 });
