cdcba9f5517cbfe2ae3b9f3eb64e5343ee5a4ce4
Thomas Cicognani <thomas.cicognani@proton.me>
2024-09-26 13:55:13 +0200

    test: fix Scanner import

--- test/filesystem/service/Scanner.test.ts
+++ test/filesystem/service/Scanner.test.ts
@@ -4,13 +4,13 @@ import { Directory } from "../../../src/filesystem/domain/valueobject/Directory.
 import { FileType } from "../../../src/filesystem/domain/valueobject/FileType.ts";
 import { Path } from "../../../src/filesystem/domain/valueobject/Path.ts";
 import { FilesRepository } from "../../../src/filesystem/repository/FilesRepository.ts";
-import { Scan } from "../../../src/filesystem/service/Scan.ts";
+import { Scanner } from "../../../src/filesystem/service/Scanner.ts";
 import { MockFilesDataAccessor } from "../../mock/repository/MockFilesDataAccessor.ts";
 
 Deno.test(async function scanAndSave() {
   const directory = new Directory(new Path("./test/resources/video-game"));
   const accessor = new MockFilesDataAccessor();
-  const service = new Scan(new FilesRepository(accessor));
+  const service = new Scanner(new FilesRepository(accessor));
   const scanData = new ScanData(directory, FileType.VIDEO_GAME);
   await service.scanAndSave(scanData);
   assertEquals(accessor.files.length, 5);
