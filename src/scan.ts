b31319fee5dcc4d6cc25df5d1a687713ba0c59e7
Thomas Cicognani <thomas.cicognani@proton.me>
2024-09-26 13:50:26 +0200

    feat: scanner now uses the configuration file

--- src/scan.ts
+++ src/scan.ts
@@ -1,16 +1,48 @@
+import type { Configuration } from "./config/domain/aggregate/Configuration.ts";
+import { FileType as ConfigurationFileType } from "./config/domain/valueobject/FileType.ts";
+import { ReadConfiguration } from "./config/service/ReadConfiguration.ts";
+import { ScanData } from "./filesystem/domain/aggregate/ScanData.ts";
 import { Directory } from "./filesystem/domain/valueobject/Directory.ts";
-import { FileType } from "./filesystem/domain/valueobject/FileType.ts";
+import { FileType as FileSystemFileType } from "./filesystem/domain/valueobject/FileType.ts";
 import { Path } from "./filesystem/domain/valueobject/Path.ts";
 import { FilesRepository } from "./filesystem/repository/FilesRepository.ts";
 import { KvAccessor } from "./filesystem/repository/KvAccessor/KvAccessor.ts";
 import { Scan } from "./filesystem/service/Scan.ts";
 
 export const scan = async (): Promise<void> => {
+  const scanData: ScanData[] = readConfiguration();
   const fileRepository = new FilesRepository(new KvAccessor());
-  const directoryToScan = new Directory(
-    new Path("./test/resources/video-game"),
-  );
-  const directoryType = FileType.VIDEO_GAME;
+  const scanner = new Scan(fileRepository);
+  for (const data of scanData) {
+    await scanner.scanAndSave(data);
+  }
+};
 
-  await new Scan(fileRepository).scanAndSave(directoryToScan, directoryType);
+const readConfiguration = (): ScanData[] => {
+  const configuration: Configuration = new ReadConfiguration().load(
+    "./config.yml",
+  );
+
+  const scanData: ScanData[] = [];
+
+  for (const scan of configuration.scans) {
+    const directoryToScan = new Directory(
+      new Path(scan.directory.rootDir.value),
+    );
+
+    const directoryType = mapFileType(scan.fileType);
+    scanData.push(new ScanData(directoryToScan, directoryType));
+  }
+
+  // TODO Also save library (video game)
+
+  return scanData;
+};
+
+const mapFileType = (fileType: ConfigurationFileType): FileSystemFileType => {
+  if (fileType === ConfigurationFileType["video-game"]) {
+    return FileSystemFileType.VIDEO_GAME;
+  }
+
+  throw new Error(`Invalid file type: ${fileType}`);
 };
