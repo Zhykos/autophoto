8b5599ff37850763b4f72ee0bbb36d1c7dba503e
Thomas Cicognani <thomas.cicognani@proton.me>
2024-10-19 01:14:58 +0200

    feat: add deno task to launch manual E2E tests

--- e2e-debug.sh
+++
@@ -1,5 +0,0 @@
-deno run --allow-read --allow-write --unstable-kv src/main.ts config.yml --scan --database=./test/e2e-debug.sqlite3
-
-deno run --allow-read --allow-write --unstable-kv src/main.ts ./test/resources/config2.yml --scan --database=./test/e2e-debug.sqlite3
-
-deno run --allow-read --allow-write --allow-net --unstable-kv src/main.ts --publish --bluesky_login="<>" --bluesky_password="<>" config.yml
