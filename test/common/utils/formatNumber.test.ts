521b75152eb5817b1fb5a5027c8975b8f175621d
AndreVale69 <sforzini31@gmail.com>
2024-12-27 17:28:57 +0100

    test: add explicit result to formatNumber.test.ts
    Solve change request.
    
    Refs: #71

--- test/common/utils/formatNumber.test.ts
+++ test/common/utils/formatNumber.test.ts
@@ -7,7 +7,7 @@ describe("Correct Format Number Utils", () => {
     // It is useful to invalidate the test if a new locale is added and not tested.
     const commonLocales: Set<string> = new Set(Object.values(CommonLocales));
     it("should format number using default, the English (United States) locale", () => {
-        const expected: string = (1000).toLocaleString("en-US");
+        const expected: string = "1,000";
         const result: string = formatNumber(1000, CommonLocales.EnglishUnitedStates);
         const resultWithDefault: string = formatNumber(1000);
         assertEquals(result, expected);
@@ -16,56 +16,56 @@ describe("Correct Format Number Utils", () => {
     });
 
     it("should format number using English (United Kingdom) locale", () => {
-        const expected: string = (1000).toLocaleString("en-GB");
+        const expected: string = "1,000";
         const result: string = formatNumber(1000, CommonLocales.EnglishUnitedKingdom);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.EnglishUnitedKingdom);
     });
 
     it("should format number using French locale", () => {
-        const expected: string = (1000).toLocaleString("fr-FR");
+        const expected: string = "1" + "\u202F" + "000"; // non-breaking space (NBSP), not equal to 1 000
         const result: string = formatNumber(1000, CommonLocales.French);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.French);
     });
 
     it("should format number using German locale", () => {
-        const expected: string = (1000).toLocaleString("de-DE");
+        const expected: string = "1.000";
         const result: string = formatNumber(1000, CommonLocales.German);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.German);
     });
 
     it("should format number using Spanish locale", () => {
-        const expected: string = (1000).toLocaleString("es-ES");
+        const expected: string = "1000";
         const result: string = formatNumber(1000, CommonLocales.Spanish);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.Spanish);
     });
 
     it("should format number using Italian locale", () => {
-        const expected: string = (1000).toLocaleString("it-IT");
+        const expected: string = "1.000";
         const result: string = formatNumber(1000, CommonLocales.Italian);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.Italian);
     });
 
     it("should format number using Japanese locale", () => {
-        const expected: string = (1000).toLocaleString("ja-JP");
+        const expected: string = "1,000";
         const result: string = formatNumber(1000, CommonLocales.Japanese);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.Japanese);
     });
 
     it("should format number using Chinese (Simplified) locale", () => {
-        const expected: string = (1000).toLocaleString("zh-CN");
+        const expected: string = "1,000";
         const result: string = formatNumber(1000, CommonLocales.Chinese);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.Chinese);
     });
 
     it("should format number using Russian locale", () => {
-        const expected: string = (1000).toLocaleString("ru-RU");
+        const expected: string = "1" + "\u00A0" + "000"; // non-breaking space
         const result: string = formatNumber(1000, CommonLocales.Russian);
         assertEquals(result, expected);
         commonLocales.delete(CommonLocales.Russian);
