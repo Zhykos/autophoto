cb37289e5e00b2e8f106df7a97c246c0607b1734
Thomas Cicognani <thomas.cicognani@proton.me>
2025-04-05 16:17:26 +0200

    ✨ feat: introduce a diagram to illustrate "Video games by year"
    #81

--- src/publishStats.ts
+++ src/publishStats.ts
@@ -6,7 +6,9 @@ import { distinct } from "@std/collections";
 import type { BlueskyStatsPublisherAction } from "./cli/domain/valueobject/BlueskyStatsPublisherAction.ts";
 import type { KvDriver } from "./common/dbdriver/KvDriver.ts";
 import { CommonKvRelationRepository } from "./common/repository/CommonKvRelationRepository.ts";
+import { CommonKvVideoGameRepository } from "./common/repository/CommonKvVideoGameRepository.ts";
 import type { VideoGameRelationImageRepositoryEntity } from "./common/repository/entity/VideoGameRelationImageRepositoryEntity.ts";
+import type { VideoGameRepositoryEntity } from "./common/repository/entity/VideoGameRepositoryEntity.ts";
 import type { UnpublishedVideoGameScreenshotRelation } from "./picker/domain/entity/UnpublishedVideoGameScreenshotRelation.ts";
 import {
   type ImageRepository,
@@ -32,10 +34,10 @@ export const publishStats = async (
   const videoGameRepository = new KvVideoGameRepository(kvDriver);
   const relationRepository = new KvRelationRepository(kvDriver);
   const relationCommonRepository = new CommonKvRelationRepository(kvDriver);
+  const videoGameCommonRepository = new CommonKvVideoGameRepository(kvDriver);
 
-  const unpublishedVideoGameRelations:
-    UnpublishedVideoGameScreenshotRelation[] = await relationRepository
-      .getUnpublishedVideoGameRelations();
+  const unpublishedVideoGameRelations: UnpublishedVideoGameScreenshotRelation[] =
+    await relationRepository.getUnpublishedVideoGameRelations();
 
   const publicationMessage: string = await statsMessage(
     imageRepository,
@@ -44,7 +46,7 @@ export const publishStats = async (
   );
 
   const diagrams: { images: Uint8Array[]; alts: string[] } =
-    await statsDiagrams(relationCommonRepository);
+    await statsDiagrams(relationCommonRepository, videoGameCommonRepository);
 
   const resultPublication: string = await new BlueskyPublisherService().publish(
     new BlueskyPublication(
@@ -123,14 +125,25 @@ ${unpublishedImagesPhrase} not published yet: it may take more ${daysWord} to pu
 
 export async function statsDiagrams(
   relationRepository: CommonKvRelationRepository,
+  videoGameRepository: CommonKvVideoGameRepository,
 ): Promise<{ images: Uint8Array[]; alts: string[] }> {
   const diagramVideoGamePlatform: { image: Uint8Array; alt: string } =
     await statsDiagramVideoGameByPlatform(relationRepository);
+  const diagramVideoGameYear: { image: Uint8Array; alt: string } =
+    await statsDiagramVideoGameByYear(videoGameRepository);
   const diagramScreenshotsPlatform: { image: Uint8Array; alt: string } =
     await statsDiagramScreenshotsByPlatform(relationRepository);
   return {
-    images: [diagramVideoGamePlatform.image, diagramScreenshotsPlatform.image],
-    alts: [diagramVideoGamePlatform.alt, diagramScreenshotsPlatform.alt],
+    images: [
+      diagramVideoGamePlatform.image,
+      diagramVideoGameYear.image,
+      diagramScreenshotsPlatform.image,
+    ],
+    alts: [
+      diagramVideoGamePlatform.alt,
+      diagramVideoGameYear.alt,
+      diagramScreenshotsPlatform.alt,
+    ],
   };
 }
 
@@ -149,18 +162,14 @@ async function statsDiagramVideoGameByPlatform(
   }
 
   const graphDefinition = `pie showData title Video games by platform
-${
-    mapPlatformVideoGameIds
-      .keys()
-      .map(
-        (k) =>
-          `"${k}" : ${
-            distinct(mapPlatformVideoGameIds.get(k) as string[]).length
-          }`,
-      )
-      .toArray()
-      .join("\n")
-  }`;
+${mapPlatformVideoGameIds
+  .keys()
+  .map(
+    (k) =>
+      `"${k}" : ${distinct(mapPlatformVideoGameIds.get(k) as string[]).length}`,
+  )
+  .toArray()
+  .join("\n")}`;
 
   const image: Uint8Array = await statsDiagramImage(graphDefinition);
 
@@ -172,20 +181,59 @@ ${
   }
 
   const alt = `Statistics about this gallery. Video games by platform are:
-${
-    mapPlatformVideoGameIds
-      .keys()
-      .map((k) => {
-        const countByPlatform: number = distinct(
-          mapPlatformVideoGameIds.get(k) as string[],
-        ).length;
-        return `  - ${k}: ${pluralFinalS(countByPlatform, "game", true)} (or ${
-          (100 * countByPlatform) / videoGamesCount
-        }%)`;
-      })
-      .toArray()
-      .join("\n")
-  }`;
+${mapPlatformVideoGameIds
+  .keys()
+  .map((k) => {
+    const countByPlatform: number = distinct(
+      mapPlatformVideoGameIds.get(k) as string[],
+    ).length;
+    return `  - ${k}: ${pluralFinalS(countByPlatform, "game", true)} (or ${
+      (100 * countByPlatform) / videoGamesCount
+    }%)`;
+  })
+  .toArray()
+  .join("\n")}`;
+
+  return { image, alt };
+}
+
+async function statsDiagramVideoGameByYear(
+  videoGameRepository: CommonKvVideoGameRepository,
+): Promise<{ image: Uint8Array; alt: string }> {
+  const allVideoGames: VideoGameRepositoryEntity[] =
+    await videoGameRepository.getAllVideoGames();
+
+  const mapYearCount = new Map<number, number>();
+  for (const vg of allVideoGames) {
+    const year: number = vg.releaseYear;
+    const yearCount: number = mapYearCount.get(year) ?? 0;
+    mapYearCount.set(year, yearCount + 1);
+  }
+
+  const graphDefinition = `pie showData title Video games by original release year
+${mapYearCount
+  .keys()
+  .map((k) => `"${k}" : ${mapYearCount.get(k)}`)
+  .toArray()
+  .join("\n")}`;
+
+  const image: Uint8Array = await statsDiagramImage(graphDefinition);
+
+  const alt = `Statistics about this gallery. Video games by original release year are:
+${mapYearCount
+  .keys()
+  .map(
+    (k) =>
+      `  - ${k}: ${pluralFinalS(
+        mapYearCount.get(k) as number,
+        "game",
+        true,
+      )} (or ${
+        (100 * (mapYearCount.get(k) as number)) / allVideoGames.length
+      }%)`,
+  )
+  .toArray()
+  .join("\n")}`;
 
   return { image, alt };
 }
@@ -203,34 +251,30 @@ async function statsDiagramScreenshotsByPlatform(
     mapPlaformCount.set(platform, count + 1);
   }
 
-  const graphDefinition =
-    `pie showData title Video games screenshots by platform
-${
-      mapPlaformCount
-        .keys()
-        .map((k) => `"${k}" : ${mapPlaformCount.get(k)}`)
-        .toArray()
-        .join("\n")
-    }`;
+  const graphDefinition = `pie showData title Video games screenshots by platform
+${mapPlaformCount
+  .keys()
+  .map((k) => `"${k}" : ${mapPlaformCount.get(k)}`)
+  .toArray()
+  .join("\n")}`;
 
   const image: Uint8Array = await statsDiagramImage(graphDefinition);
 
-  const alt =
-    `Statistics about this gallery. Video games screenshots by platform are:
-${
-      mapPlaformCount
-        .keys()
-        .map(
-          (k) =>
-            `  - ${k}: ${
-              pluralFinalS(mapPlaformCount.get(k) as number, "image", true)
-            } (or ${
-              (100 * (mapPlaformCount.get(k) as number)) / allRelations.length
-            }%)`,
-        )
-        .toArray()
-        .join("\n")
-    }`;
+  const alt = `Statistics about this gallery. Video games screenshots by platform are:
+${mapPlaformCount
+  .keys()
+  .map(
+    (k) =>
+      `  - ${k}: ${pluralFinalS(
+        mapPlaformCount.get(k) as number,
+        "image",
+        true,
+      )} (or ${
+        (100 * (mapPlaformCount.get(k) as number)) / allRelations.length
+      }%)`,
+  )
+  .toArray()
+  .join("\n")}`;
 
   return { image, alt };
 }
